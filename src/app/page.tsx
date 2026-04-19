80d74b1e1bd54a065846e179b1b3f182160b7767
ttttom.1 <chanhotom@gmail.com>
2026-04-19 20:36:10 +0900

    fix: 박-날짜 간격 절반 축소

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -399,7 +399,7 @@ export default function LandingPage() {
           alt="5월 5일 5후 5시"
           width={1085}
           height={292}
-          className="select-none w-[70%] sm:w-[60%] md:w-[50%] max-w-[380px] h-auto -mt-[12vw] sm:-mt-[8vw] md:-mt-[5vw]"
+          className="select-none w-[70%] sm:w-[60%] md:w-[50%] max-w-[380px] h-auto -mt-[18vw] sm:-mt-[12vw] md:-mt-[8vw]"
           draggable={false}
         />
 
